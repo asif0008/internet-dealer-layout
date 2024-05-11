883383a665082475d6fa2947d3c7f8f65e1af9d9
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-05-11 22:24:00 +0500

    changes made against form in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // JavaScript to handle form navigation
 const form = document.getElementById("multiStepForm");
 const steps = Array.from(form.querySelectorAll(".step"));
+const backBtn = document.getElementById('backBtn');
 
 let currentStep = 0;
+let formData = {};
 
 const updateStepVisibility = () => {
   steps.forEach((step, index) => {
@@ -30,14 +32,59 @@ const goToNextStep = () => {
     currentStep++;
     updateStepVisibility();
     updateProgressBar();
+
+    if(currentStep == 5) {
+        console.log('zipcode');
+        const zipcode = document.getElementById('zipcode');
+        zipcode.addEventListener('keydown', (e) => {
+
+            const keyCode = event.keyCode || event.which;
+
+            // Allow only numeric keys (0-9) and specific control keys (e.g., backspace, delete, arrow keys)
+            const allowedKeys = [8, 9, 37, 39, 46]; // Backspace, Tab, Left arrow, Right arrow, Delete
+            if ((keyCode < 48 || keyCode > 57) && !allowedKeys.includes(keyCode)) {
+                // Prevent default action (typing)
+                e.preventDefault();
+            }
+
+
+            setTimeout(() => {
+                const zipCodeValue = e.target.value;
+                [...formData['Zipcode'] = zipCodeValue];
+            }, 500);
+        })
+    }
   }
 };
 
+const goToPreviousStep = () => {
+    if(currentStep > 0) {
+        currentStep--;
+        updateStepVisibility();
+        updateProgressBar();
+    }
+}
+
+backBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        goToPreviousStep();
+    }, 500);
+})
+
 // Event listeners for radio buttons
-form.querySelectorAll(".btn-next").forEach((btn) => {
-    btn.addEventListener("click", () => {
+form.querySelectorAll(".go-to-next").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
     setTimeout(() => {
       goToNextStep();
     }, 500);
+
+    const name = e.target.id;
+    const value = e.target.value;
+
+    formData[name] = value;
+    console.log('formdata:', formData);
+
   });
 });
+
+
